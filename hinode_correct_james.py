fab389fde72538d3ba46a91a0b497cad9fb02a36
jameswcrowley <62038127+jameswcrowley@users.noreply.github.com>
2021-11-16 21:36:22 -0700

    Update hinode_correct_james.py

--- hinode_correct_james.py
+++ hinode_correct_james.py
@@ -10,24 +10,25 @@ import sys
 # otherwise, this part is the same.
 cube_name = sys.argv[1]
 output_cube_name = sys.argv[2]
+sir = sys.argv[3]
 
 stokes = fits.open(cube_name)[0]
 SPBSHIFT = stokes.header[103]
 stokes = stokes.data
-stokes = stokes.astype('double')
+stokes = stokes.astype('float')
 
 
 stokesI = np.copy(stokes[:,:,0,:])
-if (SPBSHFT > 0):
+if (SPBSHIFT > 0):
 	stokesI *= 2
 negative = np.where(stokesI < 0.0)
 stokesI[negative] += 65536.
 stokes[:,:,0,:] = stokesI
 
-if (SPBSHFT > 1):
+if (SPBSHIFT > 1):
 	stokes[:,:,3,:] *= 2
 
-if (SPBSHFT > 2):
+if (SPBSHIFT > 2):
 	stokes[:,:,1,:] *= 2
 	stokes[:,:,2,:] *= 2
 
@@ -37,13 +38,16 @@ stokes_corrected = np.copy(stokes)
 for i in range(stokes_corrected.shape[0]):
     for j in range(stokes_corrected.shape[1]):
         for l in range(stokes_corrected.shape[2]):
-            if (stokes[i,j, 0, l] > (stokes[i,j,0,10] + 7000.)): # to figure out what is "core"
-                stokes_corrected[i,j, 0, l] = stokes_corrected[i,j, 0, l] - 65536 
-                
+            #if (stokes[i,j, 0, l] > (stokes[i,j,0,10] + 7000.)): # to figure out what is "core"
+            if (stokes[i,j, 0, l] < 0):
+                stokes_corrected[i,j, 0, l] = stokes_corrected[i,j, 0, l] + 65536 
             else:
                 stokes_corrected[i,j, 0, l] = stokes[i,j, 0, l]
-        stokes_corrected[i,j,0,:] += 65536
+        #stokes_corrected[i,j,0,:] += 65536
 
+stokes_corrected = np.swapaxes(np.swapaxes(stokes_corrected[:, 1:, :, :60], 0, 2), 1, 3)
+for i in range(4):
+    stokes_corrected[i] = np.true_divide(stokes_corrected[i], np.mean(stokes_corrected[i, :10, :, :]))
 
 xxx = fits.PrimaryHDU(stokes_corrected)
 xxx.writeto(output_cube_name,overwrite=True)
